494129af702599916805fd234a46135fbe7bc3db
akshaynexus <akshaycm@hotmail.com>
2020-03-09 15:56:08 +0300

    Push back activation height for dgwavg

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -100,7 +100,7 @@ public:
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
         consensus.nDGWPoSHeight = 37600;//Use DGW Difficulty adjustment for PoS blocks after this height
-        consensus.nLargerDGWAvgHeight = 43500;//Use 40 blocks as average after this blockHeight
+        consensus.nLargerDGWAvgHeight = 43290;//Use 40 blocks as average after this blockHeight
         consensus.nRuleChangeActivationThreshold = 10260; // 95% of 10800
         consensus.nMinerConfirmationWindow = 10800; // nPowTargetTimespan / nPowTargetSpacing
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
